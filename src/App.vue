d8427a91a646c7804387d1451f51a86d29ef6780
wsVIC <hyf1598159330@163.com>
2023-05-31 13:50:12 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
-    <HelloWorld msg="Welcome to wsVIC's Vue.js App Yeah in test branch" />
+    <HelloWorld msg="Welcome to wsVIC's Vue.js App Yeah in test branch yet another in main" />
   </div>
 </template>
 
