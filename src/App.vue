461798ca5bf894af0fa4948e78a0f264dbed0249
wsVIC <hyf1598159330@163.com>
2023-05-31 13:30:11 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
-    <HelloWorld msg="Welcome to wsVIC's Vue.js App" />
+    <HelloWorld msg="Welcome to wsVIC's Vue.js App Yeah" />
   </div>
 </template>
 
